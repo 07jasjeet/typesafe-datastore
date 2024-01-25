66bb1f235d42f025d4c3484830ce2901af7e18a5
Jasjeet Singh <98077881+07jasjeet@users.noreply.github.com>
2024-01-26 01:26:01 +0530

    Added maven-publishing plugin

--- typesafe-datastore/build.gradle.kts
+++ typesafe-datastore/build.gradle.kts
@@ -1,7 +1,10 @@
+import com.vanniktech.maven.publish.AndroidSingleVariantLibrary
+import com.vanniktech.maven.publish.SonatypeHost
+
 plugins {
     id("com.android.library")
     id("org.jetbrains.kotlin.android")
-    id("maven-publish")
+    id("com.vanniktech.maven.publish") version "0.27.0"
 }
 
 android {
@@ -31,12 +34,6 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
-    publishing {
-        singleVariant("release") {
-            withSourcesJar()
-            withJavadocJar()
-        }
-    }
 }
 
 dependencies {
@@ -44,26 +41,47 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
 }
 
-publishing {
-    publications {
-        create<MavenPublication>("release") {
-            groupId = "com.github.07jasjeet"
-            artifactId = "typesafe-datastore"
-            version = "1.0"
-            
-            pom {
-                developers {
-                    developer {
-                        id = "Jasjeet"
-                        name = "Jasjeet Singh"
-                        email = "07jasjeet@gmail.com"
-                    }
-                }
+@Suppress("UnstableApiUsage")
+mavenPublishing {
+    configure(AndroidSingleVariantLibrary(
+        // the published variant
+        variant = "release",
+        // whether to publish a sources jar
+        sourcesJar = true,
+        // whether to publish a javadoc jar
+        publishJavadocJar = true,
+    ))
+    
+    publishToMavenCentral(SonatypeHost.S01)
+    
+    signAllPublications()
+    coordinates("com.github.07jasjeet", "typesafe-datastore", "1.0-SNAPSHOT")
+    
+    pom {
+        name.set(project.name)
+        description.set("TypeSafeDataStore is a lightweight abstraction layer on top of SharedPreferences " +
+                "DataStore that provides type-safety without dealing with Proto-DataStore.")
+        inceptionYear.set("2024")
+        url.set("https://github.com/07jasjeet/TypeSafe-DataStore/")
+        licenses {
+            license {
+                name.set("The Apache License, Version 2.0")
+                url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                distribution.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
             }
-            
-            afterEvaluate {
-                from(components["release"])
+        }
+        developers {
+            developer {
+                id = "Jasjeet"
+                name = "Jasjeet Singh"
+                email = "07jasjeet@gmail.com"
             }
+        }
+        
+        scm {
+            url.set("https://github.com/07jasjeet/TypeSafe-DataStore/")
+            connection.set("scm:git:git://github.com/07jasjeet/TypeSafe-DataStore.git")
+            developerConnection.set("scm:git:ssh://git@github.com/07jasjeet/TypeSafe-DataStore.git")
         }
     }
 }
