0d37881d6510e447af98093cf230aa690e1a9c02
Jasjeet Singh <98077881+07jasjeet@users.noreply.github.com>
2024-01-25 22:11:04 +0530

    Maven publishing setup

--- typesafe-datastore/build.gradle.kts
+++ typesafe-datastore/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.library")
     id("org.jetbrains.kotlin.android")
+    id("maven-publish")
 }
 
 android {
@@ -17,10 +18,10 @@ android {
     buildTypes {
         release {
             isMinifyEnabled = true
-            /*proguardFiles(
+            proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
-            )*/
+            )
         }
     }
     compileOptions {
@@ -30,15 +31,42 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+    publishing {
+        singleVariant("release") {
+            withSourcesJar()
+            withJavadocJar()
+        }
+    }
+    testFixtures {
+        enable = true
+    }
 }
 
 dependencies {
     implementation("androidx.datastore:datastore-preferences:1.0.0")
-    
-    implementation("androidx.core:core-ktx:1.12.0")
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.11.0")
     testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("release") {
+            groupId = "com.jasjeet"
+            artifactId = "typesafe-datastore"
+            version = "1.0"
+            
+            pom {
+                developers {
+                    developer {
+                        id = "Jasjeet"
+                        name = "Jasjeet Singh"
+                        email = "07jasjeet@gmail.com"
+                    }
+                }
+            }
+            
+            afterEvaluate {
+                from(components["release"])
+            }
+        }
+    }
 }
